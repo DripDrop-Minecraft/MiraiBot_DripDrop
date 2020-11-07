f4bc5d4071a0386fcf2087cc21dfed79a6ca0ce6
Git_a_Live <Aquarius_Pan@outlook.com>
2020-11-08 00:44:26 +0800

    commands updated

--- Mirai/src/main/kotlin/club/txyminecraft/MiRaiBot.kt
+++ Mirai/src/main/kotlin/club/txyminecraft/MiRaiBot.kt
@@ -10,6 +10,11 @@ import net.mamoe.mirai.join
 
 suspend fun main() {
     val mcPing = MinecraftPing()
+    val ip1 = "txyminecraft.club:25565"
+    val ip2 = "http://txyminecraft.club:8123"
+    val ip3 = "https://git-a-live.github.io/item_wiki.github.io/"
+    val ip4 = "https://git-a-live.github.io/itas.github.io/"
+
     println("QQ号：")
     val qqId = readLine()?.toLong()//Bot的QQ号
     println("密码：")
@@ -17,34 +22,35 @@ suspend fun main() {
     val miraiBot = Bot(qqId!!, password).alsoLogin()//新建Bot并登录
     miraiBot.subscribeMessages {
 
-        (contains("#ip") or
-         contains("#IP") or
-         contains("#地址")) {
-            reply("服务器IP：txyminecraft.club:25565")
+        (contains("#ip") or contains("#地址")) {
+            reply("服务器IP：${ip1}")
         }
 
-        (contains("#map") or
-         contains("#MAP") or
-         contains("#地图")) {
-            reply("卫星地图：http://txyminecraft.club:8123/index.html"
-                    + "\n(推荐使用QueDe Map for Android)")
+        (contains("#map") or contains("#地图")) {
+            reply("卫星地图：${ip2}\n(推荐使用QueDe Map)")
         }
 
-        (contains("#svr") or
-         contains("#SVR") or
-         contains("#服务器")) {
+        (contains("#svr") or contains("#服务器")) {
             reply(getServerInfo(mcPing))
+        }
+
+        (contains("#wiki") or contains("#百科")) {
+            reply("物品维基：${ip3}")
+        }
+
+        (contains("#faq") or contains("#问题")) {
+            reply("常见问题猛戳这里：${ip4}")
         }
     }
     miraiBot.join() // 等待 Bot 离线, 避免主线程退出
 }
 
-private fun getServerInfo(mcPing: MinecraftPing): String{
+private fun getServerInfo(mcPing: MinecraftPing?): String {
     val sb = StringBuilder()
-    val mcPR = mcPing.getPing("txyminecraft.club")
-    if (mcPing == null){
+    if (mcPing == null) {
         return "暂时无法获取服务器信息"
     } else {
+        val mcPR = mcPing.getPing("txyminecraft.club")
         try {
             runBlocking {
                 sb.append("在线玩家：")
